a82991e7389e9a4226c83d385cff2c2f7e0f4cf1
zeeshanhanif <zeeshanhanif@gmail.com>
2023-01-12 23:48:52 +0500

    Update script to claim

--- scripts/05_claim.ts
+++ scripts/05_claim.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from 'ethers';
 import { ethers } from 'hardhat';
 import { NusicStreamCoupon, NusicStreamCoupon__factory } from '../typechain';
 const addresses = require("./address.json");
@@ -5,7 +6,7 @@ const addresses = require("./address.json");
 * Main deployment script to deploy all the relevent contracts
 */
 async function main() {
-  const [owner, addr1] = await ethers.getSigners();
+  const [owner, addr1, addr2] = await ethers.getSigners();
 
   const network = addresses.localhost;
 
@@ -14,18 +15,23 @@ async function main() {
   const nusicStreamCoupon:NusicStreamCoupon = await NusicStreamCoupon.attach(network.nusicStreamCoupon);
   console.log("NusicStreamCoupon Address: ", nusicStreamCoupon.address);
 
-  let streamCount = 45;
+  let streamCount = 1000;
   //let timestamp = (new Date()).getTime();
   let timestamp = 111;
+  //let configId = 345;
+  let configId = BigNumber.from("93421307069507772285639416580684576036220280475292651263962894856226727966318");
+  
+  let tokenIdInContract = 0;
+  //let fractions = 2;
   // Signature generations
   let messageHash = ethers.utils.solidityKeccak256(
-    ["address","uint","uint", "address"],
-    [addr1.address,streamCount, timestamp, nusicStreamCoupon.address]
+    ["address","uint","address","uint","uint", "uint"],
+    [addr1.address,configId, network.nftMock, tokenIdInContract, streamCount, timestamp]
   );
   let messageHashBinary = ethers.utils.arrayify(messageHash);
   let signature = await owner.signMessage(messageHashBinary);
 
-  const txt = await nusicStreamCoupon.connect(addr1).claim(45,111,signature);
+  const txt = await nusicStreamCoupon.connect(addr1).claim(configId,network.nftMock, tokenIdInContract,streamCount,timestamp,signature);
   console.log("txt.hash = ",txt.hash);
   const txtReceipt = await txt.wait();
 }
